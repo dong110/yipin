a781728ed9121b471d6c528380acdee11c14b989
kongdongdong <1003402938@qq.com>
2015-12-31 22:47:46 +0800

    a

--- src/com/dong/yiping/utils/NetRunnable.java
+++ src/com/dong/yiping/utils/NetRunnable.java
@@ -13,14 +13,17 @@ public class NetRunnable implements Runnable{
 	private String url;
 	private String result;
 	private Map<String, String> paramMap;
+	private ToperJson toperJson;
 	public NetRunnable(Handler mHandler, String url, Map<String, String> paramMap) {
 		this.mHandler = mHandler;
 		this.url = url;
 		this.paramMap = paramMap;
+		toperJson = new ToperJson(mHandler);
 	}
 	public NetRunnable(Handler mHandler, String url) {
 		this.mHandler = mHandler;
 		this.url = url;
+		toperJson = new ToperJson(mHandler);
 	}
 	@Override
 	public void run() {
@@ -32,10 +35,7 @@ public class NetRunnable implements Runnable{
 		if(result == null){
 			mHandler.sendEmptyMessage(Constant.NET_ERROR);
 		}else{
-			Message msg = mHandler.obtainMessage();
-			msg.what = Constant.NET_SUCCESS;
-			msg.obj = result;
-			mHandler.sendMessage(msg);
+			toperJson.toperLoginJson(result);
 		}
 	}
 
