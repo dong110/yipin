9ac507080fc3ddf9bedcd59fcea3f6191c2c2ba1
mahuanh <488238623@qq.com>
2015-12-31 23:32:35 +0800

    xx

--- gen/com/dong/yiping/R.java
+++ gen/com/dong/yiping/R.java
@@ -8,36 +8,40 @@
 package com.dong.yiping;
 
 public final class R {
+    public static final class anim {
+        public static final int center_to_left=0x7f040000;
+        public static final int right_to_center=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f040000;
-        public static final int fragment_one_bg=0x7f040008;
-        public static final int gray=0x7f040007;
-        public static final int gray_text_selector=0x7f040009;
-        public static final int login_gold=0x7f040006;
-        public static final int one_blue=0x7f040005;
-        public static final int orange_text_selector=0x7f04000a;
-        public static final int red=0x7f040002;
-        public static final int tabs_text_selector=0x7f04000b;
-        public static final int title_bg=0x7f040003;
-        public static final int writer=0x7f040001;
-        public static final int yello=0x7f040004;
+        public static final int black=0x7f050000;
+        public static final int fragment_one_bg=0x7f050008;
+        public static final int gray=0x7f050007;
+        public static final int gray_text_selector=0x7f050009;
+        public static final int login_gold=0x7f050006;
+        public static final int one_blue=0x7f050005;
+        public static final int orange_text_selector=0x7f05000a;
+        public static final int red=0x7f050002;
+        public static final int tabs_text_selector=0x7f05000b;
+        public static final int title_bg=0x7f050003;
+        public static final int writer=0x7f050001;
+        public static final int yello=0x7f050004;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
-        public static final int text_margin=0x7f050003;
-        public static final int text_size=0x7f050002;
-        public static final int text_size_14=0x7f050004;
-        public static final int text_size_15=0x7f050005;
-        public static final int text_size_16=0x7f050006;
-        public static final int text_size_17=0x7f050007;
-        public static final int text_size_18=0x7f050008;
-        public static final int text_size_19=0x7f050009;
-        public static final int text_size_20=0x7f05000a;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
+        public static final int text_margin=0x7f060003;
+        public static final int text_size=0x7f060002;
+        public static final int text_size_14=0x7f060004;
+        public static final int text_size_15=0x7f060005;
+        public static final int text_size_16=0x7f060006;
+        public static final int text_size_17=0x7f060007;
+        public static final int text_size_18=0x7f060008;
+        public static final int text_size_19=0x7f060009;
+        public static final int text_size_20=0x7f06000a;
     }
     public static final class drawable {
         public static final int apply_search_bg=0x7f020000;
@@ -49,80 +53,82 @@ public final class R {
         public static final int applyinfo_img=0x7f020006;
         public static final int applyinfo_line=0x7f020007;
         public static final int common_bg=0x7f020008;
-        public static final int common_footer_bg=0x7f020009;
-        public static final int common_hearder_sharp_left=0x7f02000a;
-        public static final int common_menu=0x7f02000b;
-        public static final int common_menu_cur=0x7f02000c;
-        public static final int common_set_btn=0x7f02000d;
-        public static final int common_set_btn2=0x7f02000e;
-        public static final int common_sharp_right=0x7f02000f;
-        public static final int dialog_bg=0x7f020010;
-        public static final int ic_launcher=0x7f020011;
-        public static final int index_sharp_right=0x7f020012;
-        public static final int load_loading=0x7f020013;
-        public static final int loading_background=0x7f020014;
-        public static final int login_bg=0x7f020015;
-        public static final int login_btn=0x7f020016;
-        public static final int login_btn_cur=0x7f020017;
-        public static final int login_content_bg=0x7f020018;
-        public static final int point_normal=0x7f020019;
-        public static final int point_seletor=0x7f02001a;
-        public static final int ponit_focus=0x7f02001b;
-        public static final int request_loading=0x7f02001c;
-        public static final int resume_content_line=0x7f02001d;
-        public static final int resume_upload=0x7f02001e;
-        public static final int search_bg=0x7f02001f;
-        public static final int star=0x7f020020;
-        public static final int text_bg=0x7f020021;
-        public static final int text_bg_selector=0x7f020022;
-        public static final int text_bg_two=0x7f020023;
-        public static final int usercenter_content_line=0x7f020024;
+        public static final int common_checkbox_seleted=0x7f020009;
+        public static final int common_checkbox_unselect=0x7f02000a;
+        public static final int common_footer_bg=0x7f02000b;
+        public static final int common_hearder_sharp_left=0x7f02000c;
+        public static final int common_menu=0x7f02000d;
+        public static final int common_menu_cur=0x7f02000e;
+        public static final int common_set_btn=0x7f02000f;
+        public static final int common_set_btn2=0x7f020010;
+        public static final int common_sharp_right=0x7f020011;
+        public static final int dialog_bg=0x7f020012;
+        public static final int ic_launcher=0x7f020013;
+        public static final int index_sharp_right=0x7f020014;
+        public static final int load_loading=0x7f020015;
+        public static final int loading_background=0x7f020016;
+        public static final int login_bg=0x7f020017;
+        public static final int login_btn=0x7f020018;
+        public static final int login_btn_cur=0x7f020019;
+        public static final int login_content_bg=0x7f02001a;
+        public static final int point_normal=0x7f02001b;
+        public static final int point_seletor=0x7f02001c;
+        public static final int ponit_focus=0x7f02001d;
+        public static final int request_loading=0x7f02001e;
+        public static final int resume_content_line=0x7f02001f;
+        public static final int resume_upload=0x7f020020;
+        public static final int search_bg=0x7f020021;
+        public static final int star=0x7f020022;
+        public static final int text_bg=0x7f020023;
+        public static final int text_bg_selector=0x7f020024;
+        public static final int text_bg_two=0x7f020025;
+        public static final int usercenter_content_line=0x7f020026;
     }
     public static final class id {
-        public static final int ad_viewPage=0x7f080019;
-        public static final int cb_regist_qiye=0x7f080009;
-        public static final int cb_regist_stu=0x7f080008;
-        public static final int dialog=0x7f08002a;
-        public static final int dialog_text=0x7f08002b;
-        public static final int et_login_pwd=0x7f080002;
-        public static final int et_login_username=0x7f080001;
-        public static final int hand=0x7f080000;
-        public static final int iv_index=0x7f080025;
-        public static final int iv_title_left=0x7f080021;
-        public static final int iv_title_right=0x7f080024;
-        public static final int listview=0x7f080020;
-        public static final int ll_dian=0x7f08001b;
-        public static final int ll_regist_address=0x7f080011;
-        public static final int ll_regist_school=0x7f080013;
-        public static final int ll_regist_typename=0x7f080014;
-        public static final int ll_title_center=0x7f080023;
-        public static final int lv_listview=0x7f08001f;
-        public static final int main_tabs_layout=0x7f080007;
-        public static final int main_viewpager=0x7f080006;
-        public static final int myResume=0x7f08001e;
-        public static final int progressBar1=0x7f08001c;
-        public static final int sp_1=0x7f080015;
-        public static final int sp_2=0x7f080016;
-        public static final int sp_3=0x7f080017;
-        public static final int tv_arrer=0x7f080029;
-        public static final int tv_cp_name=0x7f080026;
-        public static final int tv_group_name=0x7f080027;
-        public static final int tv_login_findpwd=0x7f080003;
-        public static final int tv_login_login=0x7f080005;
-        public static final int tv_login_regist=0x7f080004;
-        public static final int tv_msg=0x7f08001a;
-        public static final int tv_regist_address=0x7f080012;
-        public static final int tv_regist_card_num=0x7f08000c;
-        public static final int tv_regist_email=0x7f080010;
-        public static final int tv_regist_phonenum=0x7f08000f;
-        public static final int tv_regist_pwd=0x7f08000d;
-        public static final int tv_regist_pwd_confirm=0x7f08000e;
-        public static final int tv_regist_stunum=0x7f080018;
-        public static final int tv_regist_type=0x7f08000a;
-        public static final int tv_regist_typename=0x7f08000b;
-        public static final int tv_salary=0x7f080028;
-        public static final int tv_title=0x7f08001d;
-        public static final int tv_title_center=0x7f080022;
+        public static final int ad_viewPage=0x7f090019;
+        public static final int dialog=0x7f09002a;
+        public static final int dialog_text=0x7f09002b;
+        public static final int et_login_pwd=0x7f090002;
+        public static final int et_login_username=0x7f090001;
+        public static final int hand=0x7f090000;
+        public static final int iv_index=0x7f090025;
+        public static final int iv_regist_qiye=0x7f090009;
+        public static final int iv_regist_stu=0x7f090008;
+        public static final int iv_title_left=0x7f090021;
+        public static final int iv_title_right=0x7f090024;
+        public static final int listview=0x7f090020;
+        public static final int ll_dian=0x7f09001b;
+        public static final int ll_regist_address=0x7f090011;
+        public static final int ll_regist_school=0x7f090013;
+        public static final int ll_regist_typename=0x7f090014;
+        public static final int ll_title_center=0x7f090023;
+        public static final int lv_listview=0x7f09001f;
+        public static final int main_tabs_layout=0x7f090007;
+        public static final int main_viewpager=0x7f090006;
+        public static final int myResume=0x7f09001e;
+        public static final int progressBar1=0x7f09001c;
+        public static final int sp_1=0x7f090015;
+        public static final int sp_2=0x7f090016;
+        public static final int sp_3=0x7f090017;
+        public static final int tv_arrer=0x7f090029;
+        public static final int tv_cp_name=0x7f090026;
+        public static final int tv_group_name=0x7f090027;
+        public static final int tv_login_findpwd=0x7f090003;
+        public static final int tv_login_login=0x7f090005;
+        public static final int tv_login_regist=0x7f090004;
+        public static final int tv_msg=0x7f09001a;
+        public static final int tv_regist_address=0x7f090012;
+        public static final int tv_regist_card_num=0x7f09000c;
+        public static final int tv_regist_email=0x7f090010;
+        public static final int tv_regist_phonenum=0x7f09000f;
+        public static final int tv_regist_pwd=0x7f09000d;
+        public static final int tv_regist_pwd_confirm=0x7f09000e;
+        public static final int tv_regist_stunum=0x7f090018;
+        public static final int tv_regist_type=0x7f09000a;
+        public static final int tv_regist_typename=0x7f09000b;
+        public static final int tv_salary=0x7f090028;
+        public static final int tv_title=0x7f09001d;
+        public static final int tv_title_center=0x7f090022;
     }
     public static final class layout {
         public static final int activity_companyinfo=0x7f030000;
@@ -152,19 +158,19 @@ public final class R {
         public static final int progressdialog_item=0x7f030018;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060001;
-        public static final int tabs_00=0x7f060003;
-        public static final int tabs_01=0x7f060004;
-        public static final int tabs_02=0x7f060005;
-        public static final int tabs_03=0x7f060006;
+        public static final int action_settings=0x7f070002;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070001;
+        public static final int tabs_00=0x7f070003;
+        public static final int tabs_01=0x7f070004;
+        public static final int tabs_02=0x7f070005;
+        public static final int tabs_03=0x7f070006;
     }
     public static final class style {
-        public static final int regist_left=0x7f070004;
-        public static final int regist_right=0x7f070003;
-        public static final int resume_text_one=0x7f070001;
-        public static final int resume_text_two=0x7f070002;
-        public static final int user_center_text=0x7f070000;
+        public static final int regist_left=0x7f080004;
+        public static final int regist_right=0x7f080003;
+        public static final int resume_text_one=0x7f080001;
+        public static final int resume_text_two=0x7f080002;
+        public static final int user_center_text=0x7f080000;
     }
 }
--- src/com/dong/yiping/MyApplication.java
+++ src/com/dong/yiping/MyApplication.java
@@ -1,5 +1,96 @@
 package com.dong.yiping;
 
-public class MyApplication {
+import android.app.Activity;
+import android.app.Application;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by huan on 2015/10/27.
+ */
+public class MyApplication extends Application {
+	public static MyApplication myApplication;
+	private static final String TAG = MyApplication.class.getName();
+
+	/**
+	 * 全局管理activity，方便退出
+	 */
+	private static List<Activity> activityList = new ArrayList<Activity>();
+
+	public static MyApplication getApplication() {
+		return myApplication;
+	}
+
+	@Override
+	public void onCreate() {
+		myApplication = this;
+		super.onCreate();
+
+		// 捕获没有捕获的异常
+		// Thread.setDefaultUncaughtExceptionHandler(new
+		// MyUncaughtExceptionHandler());
+
+	}
+
+	// 捕获处理器
+	private class MyUncaughtExceptionHandler implements
+			Thread.UncaughtExceptionHandler {
+
+		@Override
+		public void uncaughtException(Thread thread, Throwable ex) {
+
+			try {
+				// 把错误日志保存到sdcard
+				File file = new File(Environment.getExternalStorageDirectory(),
+						"carch.txt");
+				PrintWriter err = new PrintWriter(file);
+				ex.printStackTrace(err);
+				err.flush();
+				err.close();
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			// 早死早超生
+			android.os.Process.killProcess(android.os.Process.myPid());
+
+		}
+	}
+
+	// 资源耗尽的时候执行
+	@Override
+	public void onTerminate() {
+		super.onTerminate();
+	}
+
+	// 添加activity的引用
+	public static void addActivity(Activity activity) {
+		activityList.add(activity);
+	}
+
+	// 退出应用的时候 清空所有的activity
+	public static void clearActivity() {
+		for (Activity activity : activityList) {
+			if (activity != null) {
+				activity.finish();
+			}
+		}
+		activityList.clear();
+		// System.exit(0);
+	}
+
+	// 及时释放activity的引用
+	public static void removeActivity(Activity activity) {
+		if (activity != null) {
+			activityList.remove(activity);
+			activity = null;
+		}
+	}
 
 }
